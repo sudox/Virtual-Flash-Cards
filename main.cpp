1a922274d3961f9cdd00b7237a12e9c6bdce806e
Michael <mjg72@zips.uakron.edu>
2013-11-18 09:57:14 -0500

    Adding main file, just to determine how git works

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Init starting values
+	//Load existing flash cards from file
+	//Output user interface
+	cout << "Welcome to Virtual Flash Cards" << endl;
+	cout << "To get started please choose a menu option below:" << endl;
+	cout << "Menu Options:" << endl;
+
+}
